626448aa1d983ea0b08f80d3f983ff3a3422b7a0
Sergio Alonso <sergio.alonso.phd@gmail.com>
2025-03-20 10:22:25 +0100

    Update bisulfite_plots.R

--- bisulfite_plots.R
+++ bisulfite_plots.R
@@ -3,37 +3,50 @@ library(ggplot2)
 library(data.table)
 library(tidyr)
 
-# Create a fake data.table for testing
+# Create a fake data.table for testing (foo)
 
-foo <- data.table(Cell_Line=sample(c("LS174T","CaCo2","HCT116","HT29"),30,replace=T),Sequence=sample(LETTERS[1:5],30,replace=T))
+foo <- data.table(Cell_Line=sample(c("LS174T","CaCo2","HCT116","HT29"),30,replace=T),Sequence=factor(1:30))
 
-for(i in 1:50) {
-  foo[,(paste0("CG_",sample(1:10000,1))) := runif(30,0,1)]
+for(i in 1:25) {
+  foo[,(paste0("CG_",sample(1:10000,1))) := runif(30,0,1)] -> foo
 }
 
+# d0 will store the real data to be analyzed
 
+d0 <- foo
 
+# melt the data for ggplot 
 
-foo <- melt(foo,variable.name = "CG")
+d0 <- melt(d0,id.vars=c("Cell_Line","Sequence"),variable.name = "CG")
+d0[,Position := gsub("CG_","",CG) %>% as.numeric()]
+d0[,CG:=sprintf("CG_%05i",Position)]
 
-foo[,Position := gsub("CG_","",CG) %>% as.numeric()]
-foo[,CG:=sprintf("CG_%05i",Position)]
+# some examples
 
-ggplot(foo) + aes(Position,Sequence) + 
+# with coordinates
+
+ggplot(d0) + aes(Position,Sequence) + 
   geom_hline(aes(yintercept = Sequence)) +
   geom_point(aes(fill=value),pch=21,size=5) +
   scale_fill_gradient("Methylation",low = "white", high="black",limits=c(0,1)) +
-  theme_light() +
   facet_wrap(~ Cell_Line,scales = "free")
 
+# homogeneous distributions of CpG sites
 
-ggplot(foo) + aes(CG,Sequence) + 
+ggplot(d0) + aes(CG,Sequence) + 
   geom_hline(aes(yintercept = Sequence)) +
   geom_point(aes(fill=value),pch=21,size=5) +
-  scale_fill_gradient("Methylation",low = "white", high="black") +
-  theme_light() +
+  scale_fill_gradient("Methylation",low = "white", high="black",limits=c(0,1)) +
   theme(axis.text.x = element_text(angle=90,hjust=1,vjust=0.5)) +
   facet_wrap(~ Cell_Line,scales = "free") + 
   xlab("")
 
+# boxplots
 
+ggplot(d0) + aes(CG,value) + geom_boxplot(aes(fill=Cell_Line),alpha=.4,outliers = F) +
+  geom_point(aes(color=Cell_Line),show.legend = F) +
+  facet_wrap(~Cell_Line) +
+  theme(axis.text.x = element_text(angle=90,hjust=1,vjust=0.5)) +
+  xlab("") +
+  ylab("Methylation")
+  
