338b741ae777d2609ae9c790b31bee9c48b61b84
Sergio Alonso <sergio.alonso.phd@gmail.com>
2025-03-24 14:44:06 +0100

    Update smoother_plots_in_Windows.R

--- smoother_plots_in_Windows.R
+++ smoother_plots_in_Windows.R
@@ -16,7 +16,18 @@ p <- ggplot(data.frame(x = 1:10, y = 1:10), aes(x, y)) + geom_point()
 
 p # does it look better on screen?
 
+# save pdf 
 ggsave("cairo_plot.pdf", plot = p, device = cairo_pdf)
+
+# try this to save a png and check which one looks better
+
+png("test.cairo.png",type="cairo",width=800,height=600) 
+p
+dev.off()
+
+png("test.cairo_png.png",type="cairo-png",width=800,height=600) 
+p
+dev.off()
 
 
 
